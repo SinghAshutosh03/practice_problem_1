551561b70d2021e43bd1016f85040eaeb42df2a3
SinghAshutosh03 <ashutoshsingh030302@gmail.com>
2025-01-18 15:04:17 +0530

    sec prog

---
+++ numtoword.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class numtoword{
+  public static void main(String[] args){
+     for(int i=0; i<10;i++){
+	if(i==0) System.out.println("zero");
+	if(i==1) System.out.println("one");
+	if(i==2) System.out.println("two");
+	if(i==3) System.out.println("three");
+	if(i==4) System.out.println("four");
+	if(i==5) System.out.println("five");
+	if(i==6) System.out.println("six");
+	if(i==7) System.out.println("seven");
+	if(i==8) System.out.println("eight");
+	if(i==9) System.out.println("nine");
+	
+
+     }
+		    
+  }
+}
